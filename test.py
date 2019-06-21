abd5226d6ffcb7d5c9b871bd5bef9109776d948d
Xinyuan Liu <lxy_pku@qq.com>
2019-06-21 19:39:45 +0800

    commit

--- test.py
+++ test.py
@@ -6,7 +6,48 @@ class decode_list:
     def __init__(self,hs):
         self.parent=[hs.PL_voc['root']]
         self.name=[hs.PL_voc['root']]
-        self.[hs.PL_voc[dataset.SOS]]
+        self.trg=[hs.PL_voc[dataset.SOS]]
+        self.pending_node=[]
+        self.pending_list=None
+        self.cur_idx=0
+        self.hs=hs
+
+    def get_pending_node(self):
+        node=self.pending_node[0]
+        self.pending_node=self.pending_node[1:]
+        return node
+    
+    def update(self,output):
+        if self.pending_list!=None:
+            if output=='EOL':
+                for node in reversed(self.pending_list):
+                    if node in self.hs.grammar:
+                        fields=self.hs.grammar[node].keys()
+                        self.pending_node=[(node,field) for field in fields]+self.pending_node
+                self.pending_list=None
+            else:
+                self.pending_list.append(output)
+        else:
+            if output in self.hs.grammar:
+                fields=self.hs.grammar[output].keys()
+                self.pending_node=[(output,field) for field in fields]+self.pending_node
+        self.cur_idx+=1
+
+    def next_step(self):
+        if self.cur_idx != 0:
+            if self.pending_list==None:
+                if len(self.pending_node)==0:
+                    # finish decoding
+                    return False
+                next_node=self.get_pending_node()
+                self.parent.append(self.hs.PL_voc[next_node[0]])
+                self.name.append(self.hs.PL_voc[next_node[1]])
+                if self.hs.grammar[next_node[0]][next_node[1]]==list:
+                    self.pending_list=[]
+            else:
+                self.parent.append(self.parent[-1])
+                self.name.append(self.name[-1])
+        return True
 
 def decode(model, src, hs):
     model.eval()
@@ -14,49 +55,19 @@ def decode(model, src, hs):
         src=torch.tensor([src]).to(device)
         pad_idx=hs.PL_voc[dataset.PAD]
         
-        #dl=decode_list()
-        parent=[hs.PL_voc['root']]
-        name=[hs.PL_voc['root']]
-        trg=[hs.PL_voc[dataset.SOS]]
+        dl=decode_list(hs)
         
-        pending_node=[]
-        pending_list=None
-        for i in range(hs.max_PL_len+1):
-            if i != 0:
-                if pending_list==None:
-                    if len(pending_node)==0:
-                        break
-                    next_node=pending_node[0]
-                    pending_node=pending_node[1:]
-                    parent.append(hs.PL_voc[next_node[0]])
-                    name.append(hs.PL_voc[next_node[1]])
-                    if hs.grammar[next_node[0]][next_node[1]]==list:
-                        pending_list=[]
-                else:
-                    parent.append(parent[-1])
-                    name.append(name[-1])
-            output = model(src, torch.tensor([parent]).to(device), torch.tensor([name]).to(device), torch.tensor([trg]).to(device))
+        while(dl.cur_idx<hs.max_PL_len+1):
+            if not dl.next_step():
+                break
+            output = model(src, torch.tensor([dl.parent]).to(device), torch.tensor([dl.name]).to(device), torch.tensor([dl.trg]).to(device))
             # TODO: Beam-Search
-            maxi=output[0][i].max(-1)[1].item()
-            _, indices=torch.sort(output[0][i], descending=True)
-            #print(maxi)
-            #print(indices[0])
+            _, indices=torch.sort(output[0][dl.cur_idx], descending=True)
+            maxi=indices[0].item()
             output=hs.PL_dict[maxi]
-            trg.append(maxi)
+            dl.trg.append(maxi)
             print(output)
-            if pending_list!=None:
-                if output=='EOL':
-                    for node in reversed(pending_list):
-                        if node in hs.grammar:
-                            fields=hs.grammar[node].keys()
-                            pending_node=[(node,field) for field in fields]+pending_node
-                    pending_list=None
-                else:
-                    pending_list.append(output)
-            else:
-                if output in hs.grammar:
-                    fields=hs.grammar[output].keys()
-                    pending_node=[(output,field) for field in fields]+pending_node
+            dl.update(output)
             input()
         
 
